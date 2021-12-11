f000f8605705b166352884ddfeb0531f6c8163d7
SamerLhiany <samer.l.htc@gmail.com>
2021-12-11 20:33:15 +0200

    style update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,28 +8,28 @@ import bg from '../../assets/footer-bg.jpg';
 import logo from '../../assets/tmovie.png';
 
 const Footer = () => {
-    return (
-        <div className="footer" style={{backgroundImage: `url(${bg})`}}>
-            <div className="footer__content container">
-                <div className="footer__content__logo">
-                    <div className="logo">
-                        <img src={logo} alt="" />
-                        <Link to="/">2021</Link>
-                    </div>
-                </div>
-                <div className="footer__content__menus">
-                
-                    <div className="footer__content__menu">
-                    There’s always an exciting new movie coming out that we all want to see, right !?     
-                      MT puts the newest HD movie previews in your hands. Browse trailers, clips, and featurettes for the biggest Hollywood blockbusters and independent cinema .
-
-
-                    </div>
-                  
-                </div>
-            </div>
-        </div>
-    );
-}
+	return (
+		<div className="footer" style={{ backgroundImage: `url(${bg})` }}>
+			<div className="footer__content container">
+				<div className="footer__content__logo">
+					<div className="logo">
+						<img src={logo} alt="" />
+						<Link to="/">2021</Link>
+					</div>
+				</div>
+				<div className="footer__content__menus">
+					<div className="footer__content__menu">
+						There’s always an exciting new movie coming out that we all want to see, right !? MT puts the
+						newest HD movie previews in your hands. Browse trailers, clips, and featurettes for the biggest
+						Hollywood blockbusters and independent cinema .
+						<footer id="main-footer">
+							<p>MT  &copy; 2021, All Rights Reserved</p>
+						</footer>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Footer;
